9ab9ba59648903a718064c8d19615758fa83cc0a
Alberto <ccastro0@ucol.mx>
2020-11-27 00:14:41 -0600

    Modificacion icono

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ toCache = [
 //Fase de instalacion de SW, almacena en cache los activos estaticos
 self.addEventListener('install',e=>{
     e.waitUntil(
-        caches.open('cache03').then(cache =>{
+        caches.open('cache04').then(cache =>{
             return cache.addAll(toCache).then(() => self.skipWaiting())
         })
         .catch(error => console.log("Fallo el registro de cache",error))
@@ -20,7 +20,7 @@ self.addEventListener('install',e=>{
 
 //Activa el SW y busca los recursos para funcionar sin conexion
 self.addEventListener('activate',e=>{
-    const cacheWhitelist = ['cache03'];
+    const cacheWhitelist = ['cache04'];
     e.waitUntil(
         caches.keys().then(cacheNombres =>{
             cacheNombres.map(cacheNombre =>{
